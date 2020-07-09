464c25a110f57d3afca7810d2d5d3caae560c31e
Dylan <dylanlarrivee@gmail.com>
2020-07-09 07:12:47 -0700

    updated background image

--- src/App.js
+++ src/App.js
@@ -11,48 +11,47 @@ const windowSize = (props) => {
   });
 }
 
-// const MobileNav = (props) => {
-//   const size = useWindowSize();
-//   if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-//     return (
-//       <Drawer className="mobile-nav" title="dLarrivee">
-//             <Navigation>
-//               <Link to="/portfolio/resume">Resume</Link>
-//                 <Link to="/portfolio/projects">Projects</Link>
-//                 <Link to="/portfolio/">Home</Link>
-//             </Navigation>
-//         </Drawer>
-//     )
-//    } else if( windowSize < 800) {
-//     return (
-//       <Drawer className="mobile-nav" title="dLarrivee">
-//             <Navigation>
-//               <Link to="/portfolio/resume">Resume</Link>
-//                 <Link to="/portfolio/projects">Projects</Link>
-//                 <Link to="/portfolio/">Home</Link>
-//             </Navigation>
-//         </Drawer>
-//     )
-//    } else {
-//      return (
-//        <div>
-//        </div>
-//      )
-//    }
-// }
+const MobileNav = (props) => {
+
+  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    return (
+      <Drawer className="mobile-nav" title="dLarrivee">
+            <Navigation>
+              <Link to="/portfolio/resume">Resume</Link>
+                <Link to="/portfolio/projects">Projects</Link>
+                <Link to="/portfolio/">Home</Link>
+            </Navigation>
+        </Drawer>
+    )
+   } else if( windowSize < 800) {
+    return (
+      <Drawer className="mobile-nav" title="dLarrivee">
+            <Navigation>
+              <Link to="/portfolio/resume">Resume</Link>
+                <Link to="/portfolio/projects">Projects</Link>
+                <Link to="/portfolio/">Home</Link>
+            </Navigation>
+        </Drawer>
+    )
+   } else {
+     return (
+       <div>
+       </div>
+     )
+   }
+}
 
 function App() {
   console.log(" window.innerWidth ",  window.innerWidth )
   
   return (
     <div style={{height: '100vh', position: 'relative'}}>
-    <Layout  style={{background: 'url(	http://static.cdn.responsys.net/i2/responsysimages/content/omcprtnr003z/seattle-waterline2.jpg) center / cover'}}>
+    <Layout  style={{background: 'url(http://static.cdn.responsys.net/i2/responsysimages/content/omcprtnr003z/seattle-waterline3.jpg) center / cover'}}>
         <Header transparent title="dLarrivee" style={{color: 'black'}}>
             <Navigation >
-                <Link style={{color: 'black'}} to="/portfolio/">Home</Link>
-                <Link style={{color: 'black'}} to="/portfolio/resume">Resume</Link>
-                <Link style={{color: 'black'}} to="/portfolio/projects">Projects</Link>
-                
+              <Link style={{color: 'black'}} to="/portfolio/">Home</Link>
+              <Link style={{color: 'black'}} to="/portfolio/resume">Resume</Link>
+              <Link style={{color: 'black'}} to="/portfolio/projects">Projects</Link>
             </Navigation>
         </Header>
         <Drawer className="mobile-nav" title="dLarrivee">
